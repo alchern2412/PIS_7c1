7134bbd20afdab8ced7313b745b0a9e230d5be99
alchern2412 <mynameislyosha2412@yandex.com>
2020-06-07 23:32:19 +0300

    Add project files.

---
+++ lab7_wcf_service/DictionaryService.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using DictionaryInterface;
+using JsonRepo;
+using Models;
+
+namespace lab7_wcf_service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DictionaryService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select DictionaryService.svc or DictionaryService.svc.cs at the Solution Explorer and start debugging.
+    public class DictionaryService : IDictionaryService
+    {
+        IPhoneDictionary dictRepository = new JsonRepository();
+        private static Random random = new Random();
+
+        public void AddRecord(string name, string number)
+        {
+            Record record = new Record(random.Next(), name, number);
+            dictRepository.AddRecord(record);
+        }
+
+        public void DelRecord(int id)
+        {
+            dictRepository.Delete(id);
+        }
+
+        public List<Record> GetRecords()
+        {
+            return dictRepository.GetRecords();
+        }
+
+        public void UpdRecord(int id, string name, string number)
+        {
+            Record record = new Record(id, name, number);
+            dictRepository.Update(record);
+        }
+    }
+}
---
+++ lab7_wcf_service/IDictionaryService.cs
@@ -0,0 +1,51 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace lab7_wcf_service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "I
+    [ServiceContract]
+    public interface IDictionaryService
+    {
+        [OperationContract]
+        List<Record> GetRecords();
+
+        [OperationContract]
+        void AddRecord(string name, string number);
+
+        [OperationContract]
+        void UpdRecord(int id, string name, string number);
+
+        [OperationContract]
+        void DelRecord(int id);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
